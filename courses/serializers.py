db40db8ffe721cca8b8abdd5dd8b6816e27d3917
max_slidzen <slidzon94@gmail.com>
2023-09-18 20:00:49 +0300

    added lessons count for course

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,13 +1,18 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from courses.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    lessons = SerializerMethodField(read_only=True)
 
     class Meta:
         model = Course
         fields = '__all__'
+
+
+    def get_lessons(self, instance):
+        return instance.lesson_set.all().count()
 
 
 class LessonSerializer(ModelSerializer):
