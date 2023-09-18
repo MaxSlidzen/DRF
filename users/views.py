d59fb86ba3600f12425a60d28966dab3f46ac3ff
max_slidzen <slidzon94@gmail.com>
2023-09-18 23:03:21 +0300

    added user serializers, views and urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserAPIViewSet(ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
