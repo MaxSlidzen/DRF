534932c9acda9ce2ebbd011fcc68d44f720ec39e
max_slidzen <slidzon94@gmail.com>
2023-09-20 14:53:24 +0300

    added custom command csu and change admins

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from courses.models import Course, Lesson, Payment
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author',)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course', 'author',)
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('payment_date', 'user', 'course', 'lesson', 'payment',)
